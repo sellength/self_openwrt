8c1ba2d9b43acf3b7f8cec6ffffa3764ce5fffba
sellength <31961006+sellength@users.noreply.github.com>
2022-05-13 23:57:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -23,5 +23,5 @@
 # Add a git clone source
 #git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxmindd
 #git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
-echo 'src-git luci https://github.com/jerrykuku/luci-app-vssr.git' >>feeds.conf.default
-echo 'src-git luci https://github.com/jerrykuku/lua-maxminddb.git' >>feeds.conf.default
+echo 'src-git luci-app-vssr https://github.com/jerrykuku/luci-app-vssr' >>feeds.conf.default
+echo 'src-git lua-maxminddb https://github.com/jerrykuku/lua-maxminddb' >>feeds.conf.default
